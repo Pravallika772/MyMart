808cb47230220b2c5829a6a8cf6f5193eb0312c8
Pravallika772 <pravalikrishna24@gmail.com>
2024-01-19 20:11:11 +0530

    Deployment is done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+
+import ProductDetails from './components/productDetails';
+import { CartProvider } from './context/cartContext';
+import Home from './components/Home';
+import Shop from './components/shop/Shop';
+import Cart from './components/Cart/Cart';
+import BigDiscount from './components/home/bigDiscount';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/header/Header';
+import { ProductProvider } from './context/productContext';
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <div>  
+    
+    <BrowserRouter>
+    <CartProvider>
+      <ProductProvider>
+      <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/shop' element={<Shop/>}></Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+      <Route path='/items' element={<BigDiscount/>}></Route>
+      <Route path="/items/:itemId" element={<ProductDetails/>}></Route>
+    </Routes>
+      </ProductProvider>
+    
+    </CartProvider>
+
+    </BrowserRouter>
+    </div>
+    
+    <ToastContainer/>
     </div>
   );
 }
 
 export default App;
+
+
+
